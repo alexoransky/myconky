e96d8c679578823b121498a821d85a0b857c7463
alex oransky <alexoransky@gmail.com>
2019-01-05 16:55:42 -0500

    Added support for streamed data

--- scripts/nd/nd.lua
+++ scripts/nd/nd.lua
@@ -8,14 +8,25 @@ nd = {}
 local cjson = require "cjson.safe"
 
 nd.cmd_tmpl = "curl -s -X GET \"http://IP_ADDR/netdata/api/v1/data?chart=CMD_NAME&after=-1&format=json\""
+nd.cmd_stream_tmpl = "curl -s -X GET \"http://127.0.0.1:19999/host/HOST_NAME/api/v1/data?chart=CMD_NAME&after=-1&format=json\""
 
 local data = "data"
 
 
 function nd.cmd(ip_addr, cmd)
-    local cmd = nd.cmd_tmpl:gsub("CMD_NAME", cmd)
+    -- if the host name is supplied, try to use streamed data to the master
+    -- if the IP address is supplied, try to fetch the data from that IP
+    local ch = ip_addr:sub(1, 1)
+    n = tonumber(ch)
+    if n == nil then
+        cmd = nd.cmd_stream_tmpl:gsub("CMD_NAME", cmd)
+        cmd = cmd:gsub("HOST_NAME", ip_addr)
+    else
+        cmd = nd.cmd_tmpl:gsub("CMD_NAME", cmd)
+        cmd = cmd:gsub("IP_ADDR", ip_addr)
+    end
 
-    return cmd:gsub("IP_ADDR", ip_addr)
+    return cmd
 end
 
 
